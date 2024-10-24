2c42956faae9bcce2a610f5ed3ad6b03574a6394
jacobmbarnard <jacob.barnard.software@gmail.com>
2024-10-23 21:39:49 -0400

    fixed bug where path to templates was failing in many circumstances due to issue with GEM_HOME

--- lib/gem_integration.rb
+++ lib/gem_integration.rb
@@ -1,17 +1,8 @@
 class GemIntegration
   private
-
-  # Sorry, Linux/Unix only right now :/
-  GEM_HOME_SHELL_CMD = "echo $GEM_HOME"
-
   public
 
-  def self.gem_home
-    results = %x(#{GEM_HOME_SHELL_CMD})
-    results.delete_suffix!("\n")
-  end
-
   def self.templates_dir_from_gem_home
-    GemIntegration.gem_home + "/gems/SRSGem-0.0.5/lib/templates"
+    File.join(File.dirname(__FILE__), 'templates')
   end
 end
